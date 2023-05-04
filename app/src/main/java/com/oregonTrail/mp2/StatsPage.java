884ff60563c4e912cd9fa4ec103abc72460431fc
Truman Godsey <t-godsey@onu.edu>
2023-05-04 18:13:59 -0400

    Stats buttons

--- app/src/main/java/com/oregonTrail/mp2/StatsPage.java
+++ app/src/main/java/com/oregonTrail/mp2/StatsPage.java
@@ -3,6 +3,8 @@ package com.oregonTrail.mp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StatsPage extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class StatsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats_page);
+
+        configureBackButton();
+    }
+
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
